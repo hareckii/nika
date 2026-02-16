97e591b82e64ee2444127561b2d3807180968e14
lesha <slashertoor@gmail.com>
2026-02-16 11:02:27 +0300

    feat(solver): put all keys into nika.ini

--- problem-solver/py/secrets_env.py
+++ problem-solver/py/secrets_env.py
@@ -1,14 +1,15 @@
-from os import getenv
+import configparser
 
-from dotenv import load_dotenv
+from pathlib import Path
 
 
-load_dotenv()
+project_root = Path(__file__).parent.parent.parent
+nika_ini_path = project_root / 'nika.ini'
+config = configparser.ConfigParser()
+config.read(nika_ini_path, encoding='utf-8')
 
-GOOGLE_CLIENT_ID = getenv("GOOGLE_CLIENT_ID")
-GOOGLE_API_KEY = getenv("GOOGLE_API_KEY")
-GOOGLE_CLIENT_SECRET = getenv("GOOGLE_CLIENT_SECRET")
-
-GMAIL_PASS = getenv("GMAIL_PASSWORD")
-
-CRYPTO_KEY = getenv("CRYPTO_KEY")
+GOOGLE_CLIENT_ID = config.get('google', 'client_id')
+GOOGLE_API_KEY = config.get('google', 'api_key')
+GOOGLE_CLIENT_SECRET = config.get('google', 'client_secret')
+GMAIL_PASS = config.get('google', 'app_password')
+CRYPTO_KEY = config.get('crypto', 'key')
